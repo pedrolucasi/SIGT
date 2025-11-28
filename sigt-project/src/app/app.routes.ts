71f19b23884eac978f366cbf924785efd6239980
pedrolucasi <pp.pedro.sb@gmail.com>
2025-11-27 23:07:24 -0300

    feat: ajustes pontuais e implementação do readme

--- sigt-project/src/app/app.routes.ts
+++ sigt-project/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { TccRegistrationComponent } from './components/tcc-registration/tcc-regi
 
 export const routes: Routes = [
   { path: '', redirectTo: 'agenda-tcc', pathMatch: 'full' },
-  { path: 'agenda-tcc', component: DashboardTccComponent, title: 'SIGT'},
+  { path: 'agenda-tcc', component: DashboardTccComponent, title: 'SIGT - Sistema Gerenciador de TCC'},
   { path: 'cadastro', component: TccRegistrationComponent, title: 'SIGT | Cadastrar TCC' },
   { path: 'cadastro-tcc', component: TccRegistrationComponent, title: 'SIGT | Cadastrar TCC' },
 ];
