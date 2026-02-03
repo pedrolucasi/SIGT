0be10d877174f5faeef9b93b31ad6f5f9537c7a3
José Júnior <josecdcjunior@gmail.com>
2026-02-03 18:04:39 -0300

    feat: controllers, service e lógica de negócio

---
+++ backend/API/src/main/java/com/sigt/API/repository/TccRepository.java
@@ -0,0 +1,6 @@
+package com.sigt.API.repository;
+
+import com.sigt.API.model.Tcc;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TccRepository extends JpaRepository<Tcc, Long> { }
